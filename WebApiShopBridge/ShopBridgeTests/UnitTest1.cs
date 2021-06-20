6b1f786b1174dc8369c396e44e53db623a92c2cc
Abhisheki <Abhisheki@iazi.ch>
2021-06-20 18:20:48 +0200

    Added solution

---
+++ WebApiShopBridge/ShopBridgeTests/UnitTest1.cs
@@ -0,0 +1,171 @@
+﻿/******************************
+ * Author : Gaurish Abhisheki
+ * Date   : 20.06.2021
+ * Tests using MSUNIT
+ *****************************/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiShopBridge.Controllers;
+using WebApiShopBridge;
+using System.Threading.Tasks;
+
+
+namespace ShopBridgeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void   TestInit()
+        {
+            try
+            {
+                //Creates Dummy recrods for test in the TEST DB
+                var context = new WebApiShopBridge.ItemDbConfig();
+                context.Database.ExecuteSqlCommand("EXEC CreateTestRecords");
+            }
+            
+            catch(Exception Exp)
+            {
+                Console.WriteLine( Exp.Message);
+            }
+            
+        }
+        [TestMethod]
+        public async Task  Test1GetItemSingleId()
+        {
+            try
+            {
+
+                int ItemId = 1;
+                WebApiShopBridge.Controllers.ItemsController itemscontroller = new WebApiShopBridge.Controllers.ItemsController();
+                var result = await itemscontroller.GetItem(ItemId);
+
+                WebApiShopBridge.Item item;
+                if (result.GetType().Equals(typeof(System.Web.Http.Results.OkNegotiatedContentResult<object>)))
+                {
+                    item = (WebApiShopBridge.Item)((System.Web.Http.Results.OkNegotiatedContentResult<object>)result).Content;
+                    Assert.AreEqual(ItemId, item.ItemId);
+                    Console.WriteLine("Getitem Single Id Passed");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("Getitem single Id Failed");
+                }
+            }
+            catch(Exception Exp)
+            {
+                Console.WriteLine("Getitem single Id Failed -> " + Exp.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test2GetItemAll()
+        {
+            try
+            {
+                WebApiShopBridge.Controllers.ItemsController itemscontroller = new WebApiShopBridge.Controllers.ItemsController();
+                var result = await itemscontroller.GetItem(null);
+
+                if (result.GetType().Equals(typeof(System.Web.Http.Results.OkNegotiatedContentResult<object>)))
+                {
+                    var items = ((System.Web.Http.Results.OkNegotiatedContentResult<object>)result).Content;
+                    int count = ((System.Collections.Generic.List<WebApiShopBridge.Item>)(items)).Count;
+                    Assert.AreEqual(count , 5);
+                    Console.WriteLine("GetItem All Passed");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("GetItem All Failed");
+                }
+            }
+            catch (Exception Exp)
+            {
+                Console.WriteLine("GetItem All Failed -> " + Exp.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test3InsertItem()
+        {
+            try
+            {
+                WebApiShopBridge.Controllers.ItemsController itemscontroller = new WebApiShopBridge.Controllers.ItemsController();
+                var result = await itemscontroller.InsertItem("TestItem", "TestDecription", 999.99);
+
+                WebApiShopBridge.Item item;
+                if (result.GetType().Equals(typeof(System.Web.Http.Results.OkNegotiatedContentResult<object>)))
+                {
+                     item = (WebApiShopBridge.Item)((System.Web.Http.Results.OkNegotiatedContentResult<object>)result).Content;
+                     Assert.AreEqual("TestItem",item.ItemName);
+                     Console.WriteLine("InsertItem Passed");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("InsertItem Failed");
+                }
+            }
+            catch (Exception Exp)
+            {
+                Console.WriteLine("InsertItem Failed -> " + Exp.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test4SaveItem()
+        {
+            try
+            {
+                WebApiShopBridge.Controllers.ItemsController itemscontroller = new WebApiShopBridge.Controllers.ItemsController();
+                var result = await itemscontroller.SaveItem(2,"TestItem", "TestDecription", 999.99);
+
+                WebApiShopBridge.Item item;
+                if (result.GetType().Equals(typeof(System.Web.Http.Results.OkNegotiatedContentResult<object>)))
+                {
+                    item = (WebApiShopBridge.Item)((System.Web.Http.Results.OkNegotiatedContentResult<object>)result).Content;
+                    Assert.AreEqual("TestItem", item.ItemName);
+                    Console.WriteLine("SaveItem Passed");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("SaveItem Failed");
+                }
+            }
+            catch (Exception Exp)
+            {
+                Console.WriteLine("SaveItem Failed -> " + Exp.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task Test5DeleteItem()
+        {
+            try
+            {
+                WebApiShopBridge.Controllers.ItemsController itemscontroller = new WebApiShopBridge.Controllers.ItemsController();
+                var result = await itemscontroller.DeleteItem(4);
+
+                if (result.GetType().Equals(typeof(System.Web.Http.Results.OkNegotiatedContentResult<String>)))
+                {
+                    var returnedstring = ((System.Web.Http.Results.OkNegotiatedContentResult<String>)result).Content;
+
+                    Assert.AreEqual("Delete Successful", returnedstring);
+                    Console.WriteLine("DeleteItem All Passed");
+                }
+                else
+                {
+                    Assert.Fail();
+                    Console.WriteLine("DeleteItem  Failed");
+                }
+            }
+            catch (Exception Exp)
+            {
+                Console.WriteLine("DeleteItem  Failed -> " + Exp.Message);
+            }
+        }
+    }
+}
---
+++ WebApiShopBridge/WebApiShopBridge/Controllers/ItemsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace WebApiShopBridge.Controllers
+{
+    public class ItemsController : ApiController
+    {
+
+       
+
+        [HttpGet]
+        [Route("api/GetItem")]
+        public async Task<IHttpActionResult> GetItem(int? itemId)
+        {
+            try
+            {
+                object result;
+                Item resultItem;
+
+                
+                //TODO : Authorization code here if any
+
+                using (var context = new ItemDbConfig())
+                {
+                    if (itemId == null)
+                    {
+                        result = context.Item.ToList();
+                    }
+                    else
+                    {
+                        resultItem = await context.Item.FindAsync(itemId);
+                        result = resultItem;
+                    }
+                }
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return ErrorAsync(ex, this.Request.RequestUri.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("api/InsertItem")]
+        public async Task<IHttpActionResult> InsertItem(String ItemName, String ItemDescription, double ItemPrice)
+        {
+
+            try
+            {
+
+                object result = null;
+
+                //TODO : Authorization code here if any
+
+                //Basic Validations
+                if (ItemName.Trim() != string.Empty)
+
+                    using (var context = new ItemDbConfig())
+                    {
+                        Item item = new Item() { ItemName = ItemName, ItemDescription = ItemDescription, ItemPrice = Convert.ToDecimal(ItemPrice) };
+
+                        context.Item.Add(item);
+
+                        //Will execute sp InsertItem 
+                        await context.SaveChangesAsync();
+
+                        result = item;
+                    }
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return ErrorAsync(ex, this.Request.RequestUri.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("api/SaveItem")]
+        public async Task<IHttpActionResult> SaveItem(int itemId, String ItemName, String ItemDescription, double ItemPrice)
+        {
+            try
+            {
+
+                object result;
+
+                //TODO : Authorization code here if any
+
+                using (var context = new ItemDbConfig())
+                {
+                    Item item = context.Item.Find(itemId);
+
+                    if (item != null)
+                    {
+                        item.ItemName = ItemName;
+                        item.ItemDescription = ItemDescription;
+                        item.ItemPrice = Convert.ToDecimal(ItemPrice);
+
+                        //Will execute sp UpdateItem 
+                        await context.SaveChangesAsync();
+                    }
+                    result = item;
+                }
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return ErrorAsync(ex, this.Request.RequestUri.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/DeleteItem")]
+        public async Task<IHttpActionResult> DeleteItem(int itemId)
+        {
+            try
+            {
+
+                object result = null;
+
+                //TODO : Authorization code here if any
+
+                using (var context = new ItemDbConfig())
+                {
+                    Item item = context.Item.Find(itemId);
+
+                    if (item != null)
+                    {
+                        context.Item.Remove(item);
+
+                        //Will execute sp DeleteItem 
+                        await context.SaveChangesAsync();
+
+                        result = item;
+                    }
+                }
+
+                if (result != null)
+                    return Ok("Delete Successful");
+                else
+                    return Ok("Cant find record");
+
+            }
+            catch (Exception ex)
+            {
+                return ErrorAsync(ex, this.Request.RequestUri.ToString());
+            }
+        }
+        protected IHttpActionResult ErrorAsync(Exception ex, string uri, object value = null, bool returnException = false)
+        {
+            string error = "";
+            try
+            {
+                //TODO : Put code here if error needs to be logged in a log file
+                return Content(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (Exception e)
+            {
+
+                return Content(HttpStatusCode.InternalServerError,  e.Message);
+            }
+
+        }
+    }
+}
